6adff646c012d2abc9b866d703dce0e7c6942451
IvanShishkin <ivandenviii@gmail.com>
2021-08-27 13:37:17 +0300

    изменил версию модуля

--- install/version.php
+++ install/version.php
@@ -1,7 +1,7 @@
 <?
 $arModuleVersion = array(
-    'VERSION' => '0.1.0',
-    'VERSION_DATE' => '2021-03-21 15:00:00',
+    'VERSION' => '1.0.0',
+    'VERSION_DATE' => '2021-08-27 10:00:00',
     'MODULE_NAME' => GetMessage('LOGGER_MODULE_NAME'),
     'MODULE_DESCRIPTION' => GetMessage('LOGGER_MODULE_DESCRIPTION')
 );
